fd179217e551db30a7a86d27c5b960095d94779a
Paula Minozzo <furquimaraujominoz.p@northeastern.edu>
2025-11-30 22:24:49 -0800

    updates - removed navbar from scoreboard

--- sudoku/src/scoreboard.jsx
+++ sudoku/src/scoreboard.jsx
@@ -14,13 +14,7 @@ export default function Scoreboard() {
                 <span className="left-text">Joe</span>
                 <span className="right-text">90 pts</span>
             </div>
-            <div className="sidenav bebas-neue-regular sidenav-font">
-                <a href="https://pminoneu.github.io/sudokumockpage/index">Home</a>
-                <a href="https://pminoneu.github.io/sudokumockpage/login_page">Login</a>
-                <a href="https://pminoneu.github.io/sudokumockpage/select_game_page/">Play</a>
-                <a href="https://pminoneu.github.io/sudokumockpage/rules/">Rules</a>
-                <a className="active" href="https://pminoneu.github.io/sudokumockpage/scoreboard/">Scoreboard</a>
-            </div>
+            
         </>
     );
 }
